9b6b40706eff39c90bdb4cd1dc6706ba0feccefa
agrama <agrama@mcb.harvard.edu>
2016-12-16 16:44:47 -0500

    few small changes in stimulus_module

--- stimulus_module.py
+++ stimulus_module.py
@@ -14,8 +14,8 @@ class StimulusModule(Process):
     def run(self):
 
         self.myapp = MyApp(self.shared)
-        self.thetas = np.arange(0,360,180)           # number of stim
-        self.thetas = np.tile(self.thetas,1)    # 3 repetitions of stimuli
+        self.thetas = np.arange(0, 360, 45)           # number of stim
+        self.thetas = np.tile(self.thetas, 3)    # 3 repetitions of stimuli
         np.random.seed(1)
         self.thetas = np.random.permutation(self.thetas)
         self.numstim = len(self.thetas)
@@ -25,14 +25,12 @@ class StimulusModule(Process):
             self.new_time = time.time()
             self.dt = self.new_time - self.last_time
             self.last_time = self.new_time
-            print(1000*self.dt)
-
             if not (self.shared.frameCount.value+1) % 10:    # present every 10th frame
                 self.shared.theta.value = self.thetas[self.numstim - self.stimcount]
                 self.stim_start_time = time.time()
                 self.myapp.update_stimulus()
                 self.last_time = time.time()
-                while self.last_time- self.stim_start_time < 2:   #present gabor for 500 ms
+                while self.last_time- self.stim_start_time < 2:   #present gabor for 2 sec
                     self.myapp.taskMgr.step()
                     self.last_time = time.time()
                 self.myapp.drawgrey()
